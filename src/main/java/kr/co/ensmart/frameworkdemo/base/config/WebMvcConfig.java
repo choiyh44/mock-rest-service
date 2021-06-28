56afb92933821bec8c92e16b9c042562995e4a84
choiyh <choiyh44@gmail.com>
2021-06-28 14:25:24 +0900

    cors 설정 적용

---
+++ src/main/java/kr/co/ensmart/frameworkdemo/base/config/WebMvcConfig.java
@@ -0,0 +1,13 @@
+package kr.co.ensmart.frameworkdemo.base.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/home").setViewName("home");
+		registry.addViewController("/").setViewName("home");
+	}
+}
